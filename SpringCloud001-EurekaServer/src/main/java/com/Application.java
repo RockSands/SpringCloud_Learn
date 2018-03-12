2fa335832f686db86aa237118fb419946bc743c5
RockSands <117019049@qq.com>
2018-03-12 13:34:20 +0800

    SpringCloud:Eureka服务注册搭建

---
+++ SpringCloud001-EurekaServer/src/main/java/com/Application.java
@@ -0,0 +1,18 @@
+package com;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @EnableEurekaServer 启动Eureka服务注册服务
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class Application {
+	// 启动后访问Eureka服务页面，http://localhost:8761/
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class)
+                    .web(true).run(args);
+    }
+}
