4e757258a5dd88328fda9904414c2d493d0517b2
RockSands <117019049@qq.com>
2018-03-19 10:04:57 +0800

    Feign

--- SpringCloud003-Feign/src/main/java/com/Application.java
+++ SpringCloud003-Feign/src/main/java/com/Application.java
@@ -10,6 +10,7 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 @EnableFeignClients
 public class Application {
     public static void main(String[] args) {
-	SpringApplication.run(Application.class, args);
+    	// http://localhost:2101/consumer
+    	SpringApplication.run(Application.class, args);
     }
 }
--- SpringCloud003-Feign/src/main/java/com/client/DcClient.java
+++ SpringCloud003-Feign/src/main/java/com/client/DcClient.java
@@ -3,7 +3,11 @@ package com.client;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient("service-dc")
+/**
+ * eureka-client标识eureka的一个服务
+ * Feign默认开启Robbin
+ */
+@FeignClient("eureka-client")
 public interface DcClient {
 
     @GetMapping("/dc")
