47cbd60d1625e73500a7fb345bd0d0ce017e0766
RockSands <117019049@qq.com>
2018-03-19 10:35:22 +0800

    修正逻辑

--- SpringCloud004-ConfigClient/src/main/java/com/Application.java
+++ SpringCloud004-ConfigClient/src/main/java/com/Application.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class Application {
-
+	// 启动后,http://localhost:1001/ 发现该服务已经注册即证明生效
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(Application.class).web(true).run(args);
 	}
--- SpringCloud004-ConfigServer/src/main/java/com/Application.java
+++ SpringCloud004-ConfigServer/src/main/java/com/Application.java
@@ -2,11 +2,11 @@ package com;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableConfigServer
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
