08a638c689439770f512245781a86cac1c0f4785
RockSands <117019049@qq.com>
2018-03-20 16:49:33 +0800

    完成Filter验证

--- SpringCloud006-ZuulApi/src/main/java/com/Application.java
+++ SpringCloud006-ZuulApi/src/main/java/com/Application.java
@@ -3,6 +3,9 @@ package com;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.filter.AccessFilter;
 
 @EnableZuulProxy
 @SpringBootApplication
@@ -10,7 +13,16 @@ public class Application {
 	public static void main(String[] args) {
 		//http://localhost:1101/${服务名称}/${方法名}
 		//http://localhost:1101/eureka-consumer/consumer
-		//http://localhost:1101/api-a/dc
+		//http://localhost:1101/api-a/dc?accessToken=token
 		new SpringApplicationBuilder(Application.class).web(true).run(args);
+	}
+	
+	/**
+	 * 注册Filter
+	 * @return
+	 */
+	@Bean
+	public AccessFilter accessFilter() {
+		return new AccessFilter();
 	}
 }
