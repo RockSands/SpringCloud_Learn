f57cf2a1e4ea9181330456df5aa809d0c9383c63
RockSands <117019049@qq.com>
2018-03-20 16:40:36 +0800

    zuul

--- SpringCloud006-ZuulApi/src/main/java/com/Application.java
+++ SpringCloud006-ZuulApi/src/main/java/com/Application.java
@@ -10,6 +10,7 @@ public class Application {
 	public static void main(String[] args) {
 		//http://localhost:1101/${服务名称}/${方法名}
 		//http://localhost:1101/eureka-consumer/consumer
+		//http://localhost:1101/api-a/dc
 		new SpringApplicationBuilder(Application.class).web(true).run(args);
 	}
 }
