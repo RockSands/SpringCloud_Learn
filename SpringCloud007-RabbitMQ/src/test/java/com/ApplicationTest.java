4e008c5745490ea23926d847003ee91239f76dd4
RockSands <117019049@qq.com>
2018-03-21 15:40:28 +0800

    RabbitMQ注册成功

---
+++ SpringCloud007-RabbitMQ/src/test/java/com/ApplicationTest.java
@@ -0,0 +1,33 @@
+package com;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@EnableBinding(value = {ApplicationTest.SinkSender.class})
+public class ApplicationTest {
+
+    @Autowired
+    private SinkSender sinkSender;
+
+    @Test
+    public void sinkSenderTester() {
+        sinkSender.output().send(MessageBuilder.withPayload("produce a message : I am a test!").build());
+    }
+
+    public interface SinkSender {
+
+        String OUTPUT = "input";
+
+        @Output(SinkSender.OUTPUT)
+        MessageChannel output();
+
+    }
+
+}
